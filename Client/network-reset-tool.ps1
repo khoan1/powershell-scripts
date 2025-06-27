8a2fa83c803d24babed2d4b9f99181083d2b8fa3
Allen Nguyen <46978993+khoan1@users.noreply.github.com>
2025-06-27 05:02:28 -0700

    Add files via upload

---
+++ Client/network-reset-tool.ps1
@@ -0,0 +1,28 @@
+﻿# network-reset-tool.ps1
+# This script performs basic network reset: release IP, renew IP, and flush DNS
+
+Write-Host "===Network Reset Tool===" -ForegroundColor Cyan
+Write-Host "This will release IP, renew IP, and flush DNS..." -ForegroundColor Gray
+
+# Function to run and show command
+function Run-NetCommand {
+    param (
+        [string]$command,    # initialize $command as string object
+        [Parameter(ValueFromRemainingArguments=$true)]   # enables array object below to accept more than 1 argument
+        [string[]]$args      # initialize $args as string array object
+    )
+    # write the command to console then run command
+    Write-Host "`n[>] Running: $command $($args -join ' ')" -ForegroundColor Yellow
+    & $command @args
+}
+
+# Step 1: Release IP
+Run-NetCommand "ipconfig" "/release"
+
+# Step 2: Renew IP
+Run-NetCommand "ipconfig" "/renew"
+
+# Step 3: Flush DNS
+Run-NetCommand "ipconfig" "/flushdns"
+
+Write-Host "`n[O] Network reset completed." -ForegroundColor Green
